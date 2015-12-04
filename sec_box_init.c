94b2b960b36f318b7a0dd1737ece47938c7cf396
xuyuu <15311330977@163.com>
2015-12-04 15:33:23 +0800

    ci by xuyuuu

--- sec_box_init.c
+++ sec_box_init.c
@@ -2,7 +2,6 @@
 #include <linux/kernel.h>
 #include <linux/security.h>
 #include <linux/delay.h>
-#include <linux/jiffies.h>
 #include <linux/types.h>
 #include <linux/list.h>
 
@@ -13,19 +12,10 @@
 
 struct security_operations * security_point;
 
-u_long sec_box_module_timestamp;
 
-#ifdef __32bit__
-static u_int sec_hook_address= 0xffffffff;
-module_param(sec_hook_address, u_int, 0644);
-MODULE_PARM_DESC(sec_hook_address, "u_int security_ops address");
-#endif
-
-#ifdef __64bit__
 static u_long sec_hook_address = 0xffffffffffffffff;
 module_param(sec_hook_address, ulong, 0644);
 MODULE_PARM_DESC(sec_hook_address, "u_long security_ops address");
-#endif
 
 static void show_install_message(void)
 {
@@ -35,17 +25,10 @@ static void show_install_message(void)
 
 static int __init sec_box_init(void)
 {
-#ifdef __64bit__
 	u_long * sec_hook_address_point;
-#else
-	u_int * sec_hook_address_point;
-#endif
-	ulong cr0;
-	sec_box_module_timestamp = jiffies;
 
 	show_install_message();
 
-#ifdef __64bit__
 	sec_hook_address_point = NULL;
 	if (sec_hook_address == 0xffffffffffffffff || sec_hook_address == 0x0)
 	{
@@ -53,24 +36,15 @@ static int __init sec_box_init(void)
 		return -1;
 	}
 	sec_hook_address_point = (u_long *)sec_hook_address;
-#else
-	sec_hook_address_point = NULL;
-	if (sec_hook_address == 0xffffffff || sec_hook_address == 0x0)
-	{
-		printk("insmod argument error. \n");	
-		return -1;
-	}
-	sec_hook_address_point = (u_int *)sec_hook_address;
-#endif
 
 	security_point = (struct security_operations *)sec_hook_address_point;
 	printk("security_point : %p\n", security_point);
 
-	/*open save switch*/
-	cr0 = sec_box_hook.clear();
+	write_cr0(read_cr0() & (~ 0x10000));
 	/* set hook */
 	sec_box_hook.sethook(security_point);
-	sec_box_hook.recover(cr0);
+
+	write_cr0(read_cr0() | 0x10000);
 
 	sec_box_accesslist.init();
 	sec_box_blacklist.init();
@@ -81,10 +55,11 @@ static int __init sec_box_init(void)
 
 static void __exit sec_box_exit(void)
 {
-	ulong cr0;
-	cr0 = sec_box_hook.clear();
+	write_cr0(read_cr0() & (~ 0x10000));
+
 	sec_box_hook.resethook(security_point);
-	sec_box_hook.recover(cr0);
+
+	write_cr0(read_cr0() | 0x10000);
 
 	sec_box_socket.destroy();
 	printk("release socket source success.\n");
---
+++ sec_box_tcpstat.c
@@ -0,0 +1,171 @@
+#include <linux/list.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+
+#include "sec_box_tcpstat.h"
+
+struct sec_box_tcpstat sec_box_tcpstat;
+static struct list_head sec_box_tcpstat_head[SEC_BOX_TCPSTAT_HASHTABLE_SIZE];
+
+static int sec_box_tcpstat_simhash(ulong i_node)
+{
+	return i_node % SEC_BOX_TCPSTAT_HASHTABLE_MASK;
+}
+
+static int sec_box_tcpstat_init(void)
+{
+	int i;
+	for (i = 0; i < SEC_BOX_TCPSTAT_HASHTABLE_SIZE; i++)
+	{
+		INIT_LIST_HEAD(&sec_box_tcpstat_head[i]);
+	}
+
+	return 0;
+}
+
+static int sec_box_tcpstat_add(struct sec_box_tcpstat_node *node)
+{
+	int h = sec_box_tcpstat_simhash(node->i_node);
+	list_add_tail(&node->head, &sec_box_tcpstat_head[h]);
+
+	return 0;
+}
+
+
+static int sec_box_tcpstat_remove(struct sec_box_tcpstat_node *node)
+{
+	int rtn = sec_box_tcpstat_ok;
+	struct sec_box_tcpstat_node *item, *nxt;
+	int h = sec_box_tcpstat_simhash(node->i_node);	
+
+	list_for_each_entry_safe(item, nxt, &sec_box_tcpstat_head[h], head)
+	{
+		if (item->i_node == node->i_node)	
+		{
+			list_del(&item->head);
+			kfree(item);
+			goto out;
+		}
+	}
+	rtn = sec_box_tcpstat_error;
+out:
+	return rtn;
+}
+
+static int sec_box_tcpstat_search(struct sec_box_tcpstat_node *node)
+{
+	int rtn = sec_box_tcpstat_ok;
+	struct sec_box_tcpstat_node *item;
+	int h = sec_box_tcpstat_simhash(node->i_node);
+
+	list_for_each_entry(item, &sec_box_tcpstat_head[h], head)
+	{
+		if(item->i_node == node->i_node)
+			goto out;
+	}
+	rtn = sec_box_tcpstat_error;
+
+out:
+	return rtn;
+}
+
+static int sec_box_tcpstat_destroy(void)
+{
+	int i;
+	struct sec_box_tcpstat_node *item, *nxt;
+	for (i = 0; i < SEC_BOX_TCPSTAT_HASHTABLE_SIZE; i++)
+	{
+		list_for_each_entry_safe(item, nxt, &sec_box_tcpstat_head[i], head)
+		{
+			list_del(&item->head);	
+			kfree(item);
+		}
+	}
+
+	return 0;
+}
+
+static int sec_box_tcpstat_intostr(ulong i_node, char *str)
+{
+	int i = 0, j = 0, sz = 0;
+	while((sz = i_node / 10) > 0)
+	{   
+		str[i++] = i_node % 10 + '0'; 
+		i_node /= 10; 
+	}   
+	str[i] = i_node + '0';
+	sz = i + 1;
+
+	while(str[i] && i >= j)
+	{   
+		str[i] = str[i] ^ str[j];
+		str[j] = str[i] ^ str[j];
+		str[i] = str[i] ^ str[j];
+		i--;
+		j++;
+	}
+
+	return sz;
+}
+
+static int sec_box_tcpstat_dump(void)
+{
+	int i;
+	mm_segment_t oldfs;
+	loff_t oldpos;
+	struct sec_box_tcpstat_node *item = NULL;
+	struct file *tmpf = NULL;
+	char kern_buff[1024], *point;
+
+	tmpf = filp_open(SEC_BOX_TCPSTAT_LOG_FILE, O_RDWR | O_CREAT | O_TRUNC, 0);
+	if(IS_ERR(tmpf))
+	{
+		tmpf = NULL;
+		printk("sec_box_tcpstat_dump has error in filp_open .\n");	
+		goto out;
+	}
+	oldfs = get_fs();
+	set_fs(KERNEL_DS);
+	oldpos = tmpf->f_pos;
+	tmpf->f_pos = 0;
+
+	for (i = 0; i < SEC_BOX_TCPSTAT_HASHTABLE_SIZE; i++)
+	{
+		if (list_empty(&sec_box_tcpstat_head[i]))
+			continue;
+		list_for_each_entry(item, &sec_box_tcpstat_head[i], head)
+		{
+			memset(kern_buff, 0, sizeof(kern_buff));
+			sec_box_tcpstat_intostr(item->i_node, kern_buff);
+
+			point = kern_buff;
+			point += strlen(kern_buff);
+			*point = '\n';
+			tmpf->f_op->write(tmpf, kern_buff, strlen(kern_buff),&tmpf->f_pos);
+		}
+	}
+
+	tmpf->f_pos = oldpos;
+	set_fs(oldfs);
+
+out:
+	if (tmpf)
+		filp_close(tmpf, 0);
+
+	return 0;
+}
+
+__attribute__((constructor))
+int sec_box_tcpstat_module_init(void)
+{
+	sec_box_tcpstat.init		= sec_box_tcpstat_init;	
+	sec_box_tcpstat.add		= sec_box_tcpstat_add;
+	sec_box_tcpstat.remove		= sec_box_tcpstat_remove;
+	sec_box_tcpstat.search		= sec_box_tcpstat_search;
+	sec_box_tcpstat.destroy		= sec_box_tcpstat_destroy;
+	sec_box_tcpstat.dump		= sec_box_tcpstat_dump;
+
+	return 0;
+}
